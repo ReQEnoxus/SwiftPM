1040179157b5d1883b8d6926b3cfcb190fc4657c
enoxus <reqenoxus@gmail.com>
2021-01-16 20:47:48 +0300

    removed executable package example

--- Projects/2. Executable Package/ColorCounter/README.md
+++
@@ -1,3 +0,0 @@
-# ColorCounter
-
-A description of this package.
--- Projects/2. Executable Package/ColorCounter/Tests/LinuxMain.swift
+++
@@ -1,7 +0,0 @@
-import XCTest
-
-import ColorCounterTests
-
-var tests = [XCTestCaseEntry]()
-tests += ColorCounterTests.allTests()
-XCTMain(tests)
